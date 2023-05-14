34f8f35d14850c6bfff0a83ac350e6a9129d155e
Sergey Egorov <ispanezzz.foto@gmail.com>
2023-05-14 10:17:53 +0300

    do base version

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ let expences = [];
 const inputExpenceNode = document.querySelector(".js-input-expences");
 const addExpenceNode = document.querySelector(".add-expence-button");
 const historyNode = document.querySelector(".js-history-list");
+const amountNode = document.querySelector(".js-amount");
+const limitNode = document.querySelector(".js-limit");
+const statusNode = document.querySelector(".js-status");
 
 // вводим затраты
 
@@ -17,10 +20,32 @@ addExpenceNode.addEventListener("click", function () {
   // Сохраняем список трат
 
   let expencesListHTML = "";
+  let amount = 0;
 
   expences.forEach((element) => {
-    expencesListHTML += `<li>${element}</li>`;
-    console.log(expencesListHTML);
+    expencesListHTML += `<li>${element} руб.</li>`;
   });
   historyNode.innerHTML = `<ol>${expencesListHTML}</ol>`;
+
+  // Считаем сумму  трат
+
+  expences.forEach((element) => {
+    amount += element;
+    amountNode.innerHTML = `<p> ${amount} руб.</p>`;
+  });
+
+  //  Задаём и выводим лимит
+
+  let limit = 10000;
+  limitNode.innerHTML = `<p>${limit} руб.</p>`;
+
+  // Проверка выхода за лимит и отображение статуса
+
+  if (amount > limit) {
+    statusNode.innerHTML = `<p>Всё плохо</p>`;
+    statusNode.classList.add("status-red");
+  } else {
+    statusNode.innerHTML = `<p>Всё хорошо</p>`;
+    statusNode.classList.remove("status-red");
+  }
 });
